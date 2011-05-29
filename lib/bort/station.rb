b637d5bd3f39dcf74e2c911e9e717b46b627b1d8
Hwan-Joon Choi <hc5duke@gmail.com>
2011-05-29 12:04:10 -0700

    Station::Access

--- lib/bort/station.rb
+++ lib/bort/station.rb
@@ -1,9 +1,52 @@
 module Bort
   module Station
-    class StationAccess
+    class Access
+      attr_accessor :origin, :legend, :parking_flag, :bike_flag,
+        :bike_station_flag, :locker_flag, :name, :abbreviation,
+        :entering, :exiting, :parking, :fill_time, :car_share,
+        :lockers, :bike_station_text, :destinations, :transit_info, :link
+
+      def initialize(orig, options={})
+        self.origin = orig
+        load_options(options)
+
+        download_options = {
+          :action => 'station',
+          :cmd => 'stnaccess',
+          :orig => origin,
+          :l => legend,
+        }
+
+        xml = Util.download(download_options)
+        data = Hpricot(xml)
+
+        self.legend             = (data/:legend).inner_text
+        station_data            = (data/:station).first
+        self.parking_flag       = station_data.attributes['parking_flag'] == '1'
+        self.bike_flag          = station_data.attributes['bike_flag'] == '1'
+        self.bike_station_flag  = station_data.attributes['bike_station_flag'] == '1'
+        self.locker_flag        = station_data.attributes['locker_flag'] == '1'
+        self.name               = (station_data/:name).inner_text
+        self.abbreviation       = (station_data/:abbr).inner_text
+        self.entering           = (station_data/:entering).inner_text
+        self.exiting            = (station_data/:exiting).inner_text
+        self.parking            = (station_data/:parking).inner_text
+        self.fill_time          = (station_data/:fill_time).inner_text
+        self.car_share          = (station_data/:car_share).inner_text
+        self.lockers            = (station_data/:lockers).inner_text
+        self.bike_station_text  = (station_data/:bike_station_text).inner_text
+        self.destinations       = (station_data/:destinations).inner_text
+        self.transit_info       = (station_data/:transit_info).inner_text
+        self.link               = (station_data/:link).inner_text
+      end
+
+      private
+      def load_options(options)
+        self.legend = options.delete(:legend)
+      end
     end
 
-    class StationInfo
+    class Info
     end
 
     class Stations
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,16 +1,32 @@
 require 'spec_helper'
 
 describe Station do
-  # StationInfo
-  # StationAccess
-  # Stations
   describe "when querying station access" do
 
     before :each do
       Util.stub!(:download).and_return(response_file('station', 'stnaccess'))
+      @access = Bort::Station::Access.new('dubl')
     end
 
     it "should parse download data" do
+      @access.origin.should == 'dubl'
+      @access.legend.length.should > 10
+      @access.parking_flag.should == false
+      @access.bike_flag.should == false
+      @access.bike_station_flag.should == false
+      @access.locker_flag.should == true
+      @access.name.should == '12th St. Oakland City Center'
+      @access.abbreviation.should == '12TH'
+      @access.entering.length.should > 10
+      @access.exiting.length.should > 10
+      @access.parking.length.should > 10
+      @access.fill_time.should == ''
+      @access.car_share.should == ''
+      @access.lockers.length.should > 10
+      @access.bike_station_text.should == ''
+      @access.destinations.length.should > 10
+      @access.transit_info.length.should > 10
+      @access.link.should == ''
     end
   end
 
